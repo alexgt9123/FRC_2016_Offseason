5d53bb860e9b554f6ccf2817c2c37cb09bb523bd
alexgt9123 <twigga04@gmail.com>
2016-09-06 23:24:15 -0400

    Added functions for extra info
    Robot mode, battery voltage, alliance, and match time

--- src/org/usfirst/frc/team5712/robot/Robot.cpp
+++ src/org/usfirst/frc/team5712/robot/Robot.cpp
@@ -36,4 +36,57 @@ class Robot : public IteritaveRobot
       wait(0.01);
     }
   }
+  
+  int getRobotMode() //0 = Disabled, 1 = Enabled, 2 = Auto, 3 = Teleop, 4 = Test
+  {
+    if(IsDisabled())
+    {
+      return 0;
+    }
+    else if(IsEnabled())
+    {
+      return 1;
+    }
+    else if(IsAutonomous())
+    {
+      return 2;
+    }
+    else if(IsOperatorControl())
+    {
+      return 3;
+    }
+    else if(IsTest())
+    {
+      return 4;
+    }
+  }
+  
+  double getBatteryVoltage()
+  {
+    return DriverStation::GetInstance().GetBatteryVoltage();
+  }
+  
+  int getAlliance() //1 = Blue, 2 = Red, 0 = None
+  {
+    DriverStation::Alliance color;
+    color = DriverStation::GetInstance().GetAlliance();
+    
+    if(color == DriverStation::Alliance::kBlue)
+    {
+      return 1;
+    }
+    else if(color == DriverStation::Alliance::kRed)
+    {
+      return 2;
+    }
+    else
+    {
+      return 0;
+    }
+    
+    double getMatchTime()
+    {
+      return DriverStation::GetInstance().GetMatchTime();
+    }
+  }
 }
